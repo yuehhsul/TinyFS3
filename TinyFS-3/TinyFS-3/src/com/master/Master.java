0126650aa8212ff4ff636119f3adc6e03ba4ac54
Yueh-Hsun Lin <yuehhsul@usc.edu>
2018-04-18 16:10:56 -0700

    copied clientFS to master
    .

--- TinyFS-3/TinyFS-3/src/com/master/Master.java
+++ TinyFS-3/TinyFS-3/src/com/master/Master.java
@@ -1,161 +1,112 @@
 package com.master;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-import java.nio.ByteBuffer;
+import com.client.FileHandle;
+import com.client.ClientFS.FSReturnVals;
 
-import com.chunkserver.ChunkServer;
-import com.client.Client;
-import com.interfaces.MasterInterface;
+public class Master {
+	public enum FSReturnVals {
+		DirExists, // Returned by CreateDir when directory exists
+		DirNotEmpty, //Returned when a non-empty directory is deleted
+		SrcDirNotExistent, // Returned when source directory does not exist
+		DestDirExists, // Returned when a destination directory exists
+		FileExists, // Returned when a file exists
+		FileDoesNotExist, // Returns when a file does not exist
+		BadHandle, // Returned when the handle for an open file is not valid
+		RecordTooLong, // Returned when a record size is larger than chunk size
+		BadRecID, // The specified RID is not valid, used by DeleteRecord
+		RecDoesNotExist, // The specified record does not exist, used by DeleteRecord
+		NotImplemented, // Specific to CSCI 485 and its unit tests
+		Success, //Returned when a method succeeds
+		Fail //Returned when a method fails
+	}
 
-/**
- * implementation of interfaces at the client side
- * @author Shahram Ghandeharizadeh
- *
- */
-public class Master implements MasterInterface {
-	
-	static int ServerPort = 0;
-	static Socket ClientSocket;
-	static ObjectOutputStream WriteOutput;
-	static ObjectInputStream ReadInput;
-	
-	public static byte[] RecvPayload(String caller, ObjectInputStream instream, int sz){
-		byte[] tmpbuf = new byte[sz];
-		byte[] InputBuff = new byte[sz];
-		int ReadBytes = 0;
-		while (ReadBytes != sz){
-			int cntr=-1;
-			try {
-				cntr = instream.read( tmpbuf, 0, (sz-ReadBytes) );
-				for (int j=0; j < cntr; j++){
-					InputBuff[ReadBytes+j]=tmpbuf[j];
-				}
-			} catch (IOException e) {
-				System.out.println("Error in RecvPayload ("+caller+"), failed to read "+sz+" after reading "+ReadBytes+" bytes.");
-				return null;
-			}
-			if (cntr == -1) {
-				System.out.println("Error in RecvPayload ("+caller+"), failed to read "+sz+" bytes.");
-				return null;
-			}
-			else ReadBytes += cntr;
-		}
-		return InputBuff;
-	}
-	
-	public static int ReadIntFromInputStream(String caller, ObjectInputStream instream){
-		int PayloadSize = -1;
-		
-		byte[] InputBuff = RecvPayload(caller, instream, 4);
-		if (InputBuff != null)
-			PayloadSize = ByteBuffer.wrap(InputBuff).getInt();
-		return PayloadSize;
-	}
-	
 	/**
-	 * Initialize the client  FileNotFoundException
+	 * Creates the specified dirname in the src directory Returns
+	 * SrcDirNotExistent if the src directory does not exist Returns
+	 * DestDirExists if the specified dirname exists
+	 *
+	 * Example usage: CreateDir("/", "Shahram"), CreateDir("/Shahram/",
+	 * "CSCI485"), CreateDir("/Shahram/CSCI485/", "Lecture1")
 	 */
-	public Master(){
-		if (ClientSocket != null) return; //The client is already connected
-		try {
-			BufferedReader binput = new BufferedReader(new FileReader(ChunkServer.ClientConfigFile));
-			String port = binput.readLine();
-			port = port.substring( port.indexOf(':')+1 );
-			ServerPort = Integer.parseInt(port);
-			
-			ClientSocket = new Socket("127.0.0.1", ServerPort);
-			WriteOutput = new ObjectOutputStream(ClientSocket.getOutputStream());
-			ReadInput = new ObjectInputStream(ClientSocket.getInputStream());
-		}catch (FileNotFoundException e) {
-			System.out.println("Error (Client), the config file "+ ChunkServer.ClientConfigFile +" containing the port of the ChunkServer is missing.");
-		}catch (IOException e) {
-			System.out.println("Can't find file.");
-		}
-	}
-	
-	/**
-	 * Create a chunk at the chunk server from the client side.
-	 */
-	public String createChunk() {
-		try {
-			WriteOutput.writeInt(ChunkServer.PayloadSZ + ChunkServer.CMDlength);
-			WriteOutput.writeInt(ChunkServer.CreateChunkCMD);
-			WriteOutput.flush();
-			
-			int ChunkHandleSize =  ReadIntFromInputStream("Client", ReadInput);
-			ChunkHandleSize -= ChunkServer.PayloadSZ;  //reduce the length by the first four bytes that identify the length
-			byte[] CHinBytes = RecvPayload("Client", ReadInput, ChunkHandleSize); 
-			return (new String(CHinBytes)).toString();
-		} catch (IOException e) {
-			System.out.println("Error in Client.createChunk:  Failed to create a chunk.");
-			e.printStackTrace();
-		} 
+	public FSReturnVals CreateDir(String src, String dirname) {
 		return null;
 	}
-	
-	/**
-	 * Write a chunk at the chunk server from the client side.
-	 */
-	public boolean writeChunk(String ChunkHandle, byte[] payload, int offset) {
-		if(offset + payload.length > ChunkServer.ChunkSize){
-			System.out.println("The chunk write should be within the range of the file, invalide chunk write!");
-			return false;
-		}
-		try {
-			byte[] CHinBytes = ChunkHandle.getBytes();
-			
-			WriteOutput.writeInt(ChunkServer.PayloadSZ + ChunkServer.CMDlength + (2*4) + payload.length + CHinBytes.length);
-			WriteOutput.writeInt(ChunkServer.WriteChunkCMD);
-			WriteOutput.writeInt(offset);
-			WriteOutput.writeInt(payload.length);
-			WriteOutput.write(payload);
-			WriteOutput.write(CHinBytes);
-			WriteOutput.flush();
-			
-			int result =  Client.ReadIntFromInputStream("Client", ReadInput);
-			if (result == ChunkServer.FALSE) return false;
-			return true;
-		} catch (IOException e) {
-			System.out.println("Error in Client.createChunk:  Failed to create a chunk.");
-			e.printStackTrace();
-		}
-		return false;
-	}
-	
-	/**
-	 * Read a chunk at the chunk server from the client side.
-	 */
-	public byte[] readChunk(String ChunkHandle, int offset, int NumberOfBytes) {
-		if(NumberOfBytes + offset > ChunkServer.ChunkSize){
-			System.out.println("The chunk read should be within the range of the file, invalide chunk read!");
-			return null;
-		}
-		
-		try {
-			byte[] CHinBytes = ChunkHandle.getBytes();
-			WriteOutput.writeInt(ChunkServer.PayloadSZ + ChunkServer.CMDlength + (2*4) + CHinBytes.length);
-			WriteOutput.writeInt(ChunkServer.ReadChunkCMD);
-			WriteOutput.writeInt(offset);
-			WriteOutput.writeInt(NumberOfBytes);
-			WriteOutput.write(CHinBytes);
-			WriteOutput.flush();
-			
-			int ChunkSize =  Client.ReadIntFromInputStream("Client", ReadInput);
-			ChunkSize -= ChunkServer.PayloadSZ;  //reduce the length by the first four bytes that identify the length
-			byte[] payload = RecvPayload("Client", ReadInput, ChunkSize); 
-			return payload;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
+	/**
+	 * Deletes the specified dirname in the src directory Returns
+	 * SrcDirNotExistent if the src directory does not exist Returns
+	 * DestDirExists if the specified dirname exists
+	 *
+	 * Example usage: DeleteDir("/Shahram/CSCI485/", "Lecture1")
+	 */
+	public FSReturnVals DeleteDir(String src, String dirname) {
 		return null;
 	}
+
+	/**
+	 * Renames the specified src directory in the specified path to NewName
+	 * Returns SrcDirNotExistent if the src directory does not exist Returns
+	 * DestDirExists if a directory with NewName exists in the specified path
+	 *
+	 * Example usage: RenameDir("/Shahram/CSCI485", "/Shahram/CSCI550") changes
+	 * "/Shahram/CSCI485" to "/Shahram/CSCI550"
+	 */
+	public FSReturnVals RenameDir(String src, String NewName) {
+		return null;
+	}
+
+	/**
+	 * Lists the content of the target directory Returns SrcDirNotExistent if
+	 * the target directory does not exist Returns null if the target directory
+	 * is empty
+	 *
+	 * Example usage: ListDir("/Shahram/CSCI485")
+	 */
+	public String[] ListDir(String tgt) {
+		return null;
+	}
+
+	/**
+	 * Creates the specified filename in the target directory Returns
+	 * SrcDirNotExistent if the target directory does not exist Returns
+	 * FileExists if the specified filename exists in the specified directory
+	 *
+	 * Example usage: Createfile("/Shahram/CSCI485/Lecture1/", "Intro.pptx")
+	 */
+	public FSReturnVals CreateFile(String tgtdir, String filename) {
+		return null;
+	}
+
+	/**
+	 * Deletes the specified filename from the tgtdir Returns SrcDirNotExistent
+	 * if the target directory does not exist Returns FileDoesNotExist if the
+	 * specified filename is not-existent
+	 *
+	 * Example usage: DeleteFile("/Shahram/CSCI485/Lecture1/", "Intro.pptx")
+	 */
+	public FSReturnVals DeleteFile(String tgtdir, String filename) {
+		return null;
+	}
+
+	/**
+	 * Opens the file specified by the FilePath and populates the FileHandle
+	 * Returns FileDoesNotExist if the specified filename by FilePath is
+	 * not-existent
+	 *
+	 * Example usage: OpenFile("/Shahram/CSCI485/Lecture1/Intro.pptx", FH1)
+	 */
+	public FSReturnVals OpenFile(String FilePath, FileHandle ofh) {
+		return null;
+	}
+
+	/**
+	 * Closes the specified file handle Returns BadHandle if ofh is invalid
+	 *
+	 * Example usage: CloseFile(FH1)
+	 */
+	public FSReturnVals CloseFile(FileHandle ofh) {
+		return null;
+	}
+
 
 }
