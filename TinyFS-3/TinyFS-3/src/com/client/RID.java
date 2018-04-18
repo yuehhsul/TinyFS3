de13437d93c42248c755777c821fe13be021ff5c
Hailey Manuel <hmanuel@usc.edu>
2018-04-18 15:47:52 -0700

    adding constructor to RID

--- TinyFS-3/TinyFS-3/src/com/client/RID.java
+++ TinyFS-3/TinyFS-3/src/com/client/RID.java
@@ -1,5 +1,13 @@
 package com.client;
 
 public class RID {
+	String chunkHandle;
+	int offset; //this is the offset of records from the end, not the number of byte
+				//an offset of 1 means it is the second to last record and i must go from the end of the 
+				//chunk handle the chunklength - offset * 4
 	
+	public RID(String chunkHandle, int offset) {
+		this.chunkHandle = chunkHandle;
+		this.offset = offset;
+	}
 }
