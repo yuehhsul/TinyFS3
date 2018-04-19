bd71e587a196e44623075815e903989ff41d1471
Yueh-Hsun Lin <yuehhsul@usc.edu>
2018-04-19 15:12:48 -0700

    base structure of filehandle

--- TinyFS-3/TinyFS-3/src/com/client/FileHandle.java
+++ TinyFS-3/TinyFS-3/src/com/client/FileHandle.java
@@ -1,5 +1,22 @@
 package com.client;
 
-public class FileHandle {
+import java.util.ArrayList;
+import java.util.Map;
 
+public class FileHandle {
+	private Map<String, String> chunkAddrMap;
+	private ArrayList<String> chunkList;
+	
+	public ArrayList<String> getChunkList() {
+		return chunkList;
+	}
+	
+	public Map<String, String> getChunkAddrMap() {
+		return chunkAddrMap;
+	}
+	
+	public void addChunk(String chunk) {
+		chunkList.add(chunk);
+		chunkAddrMap.put(chunk, "");
+	}
 }
